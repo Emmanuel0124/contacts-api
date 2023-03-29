0e1015bf2cb06cebc205861fe3558ccbe8a8f693
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-29 12:46:52 -0500

    show contact  & show all contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,4 +9,40 @@ class ContactsController < ApplicationController
       phone_number: contact.phone_number
     }
   end
+
+  def all 
+    contacts = Contact.all
+    all_contacts = []
+    contacts.each do |contact|
+      all_contacts << {
+        id: contact.id,
+        first_name: contact.first_name,
+        last_name: contact.last_name,
+        email: contact.email,
+        phone_number: contact.phone_number
+      }
+    end 
+    render json: [
+      {
+        id: contacts[0].id,
+        first_name: contacts[0].first_name,
+        last_name: contacts[0].last_name,
+        email: contacts[0].email,
+        phone_number: contacts[0].phone_number
+      },
+      {
+        id: contacts[1].id,
+        first_name: contacts[1].first_name,
+        last_name: contacts[1].last_name,
+        email: contacts[1].email,
+        phone_number: contacts[1].phone_number
+      }
+
+
+
+
+
+    ]
+
+  end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "/one_contact" => "contact#show"
+  get "/one_contact" => "contacts#show"
+  get "all_contacts" => "contacts#all"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
